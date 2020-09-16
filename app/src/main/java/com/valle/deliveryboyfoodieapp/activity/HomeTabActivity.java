5e737792b6f5eac21896b5f7261df6935fa65694
HanishRanaAndroid <hanishrana53@gmail.com>
2020-09-16 16:36:38 +0530

    new

--- app/src/main/java/com/valle/deliveryboyfoodieapp/activity/HomeTabActivity.java
+++ app/src/main/java/com/valle/deliveryboyfoodieapp/activity/HomeTabActivity.java
@@ -41,7 +41,7 @@ public class HomeTabActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_tab);
-
+//
         fragmentManager = getSupportFragmentManager();
         tvSignOut = findViewById(R.id.tvSignOut);
         bottomNavigationView = findViewById(R.id.navigation);
